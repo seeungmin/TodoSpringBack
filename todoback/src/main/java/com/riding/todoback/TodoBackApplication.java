df051d9fd810d136c828ce6660b3092ed87a7b00
Lee <lsmin476@naver.com>
2023-02-27 21:27:00 +0900

    |MAKE| Spring Project 0227

---
+++ todoback/src/main/java/com/riding/todoback/TodoBackApplication.java
@@ -0,0 +1,13 @@
+package com.riding.todoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackApplication.class, args);
+	}
+
+}
---
+++ todoback/src/test/java/com/riding/todoback/TodobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riding.todoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
