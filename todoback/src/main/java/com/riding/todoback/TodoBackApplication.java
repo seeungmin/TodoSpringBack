46d361bb094cfa1ab54f77f5ff36315a1a99d9d8
Lee <lsmin476@naver.com>
2023-03-14 04:38:07 +0900

    |UPDATE| Spring Project ec2 server mariadb connect 0314

--- todoback/src/main/java/com/riding/todoback/TodoBackApplication.java
+++ todoback/src/main/java/com/riding/todoback/TodoBackApplication.java
@@ -2,8 +2,6 @@ package com.riding.todoback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class TodoBackApplication {
