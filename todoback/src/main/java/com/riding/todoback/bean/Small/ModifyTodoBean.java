e48f6f311f00e125eeb83b04f5eb82662538cad9
Lee <lsmin476@naver.com>
2023-03-16 23:21:26 +0900

    |MAKE| Spring Project modifytodo make 0316

---
+++ todoback/src/main/java/com/riding/todoback/bean/Small/ModifyTodoBean.java
@@ -0,0 +1,30 @@
+package com.riding.todoback.bean.Small;
+
+import com.riding.todoback.entity.TodoEntity;
+import com.riding.todoback.repository.TodoRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ModifyTodoBean {
+
+    @Autowired
+    TodoRepositoryJPA todoRepositoryJPA;
+
+    // 할 일 데이터 수정
+    public void exec(long id, String content){
+
+        // 아이디로 수정할 할 일 찾기
+        TodoEntity todoEntity = todoRepositoryJPA.findById(id).get();
+
+        // 새로 받을 내용 작성
+        todoEntity.setContent(content);
+        todoEntity.setUploadTime(LocalDateTime.now());
+
+        // 새로운 데이터 기존 아이디에 저장
+        todoRepositoryJPA.save(todoEntity);
+    }
+
+}
