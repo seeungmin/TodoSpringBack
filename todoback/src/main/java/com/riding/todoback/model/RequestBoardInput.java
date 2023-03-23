7e2e12d952675c132af0abc4db136f40ccdfbc08
Lee <lsmin476@naver.com>
2023-03-23 22:14:04 +0900

    |UPDATE| Spring Project boardInput and todoInput DTO modify update 0323

--- todoback/src/main/java/com/riding/todoback/model/RequestBoardInput.java
+++ todoback/src/main/java/com/riding/todoback/model/RequestBoardInput.java
@@ -2,11 +2,7 @@ package com.riding.todoback.model;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class RequestBoardInput {
-    Long id;
     String content;
-    LocalDateTime uploadTime;
 }
