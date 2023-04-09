20b6c79bbaee0960b02bb082e9df89ed05fcf646
Lee <lsmin476@naver.com>
2023-04-09 13:25:06 +0900

    |UPDATE| Spring Project show preview cash board small bean update 0409

--- todoback/src/main/java/com/riding/todoback/bean/ShowPreviewCashBoardBean.java
+++ todoback/src/main/java/com/riding/todoback/bean/ShowPreviewCashBoardBean.java
@@ -1,38 +1,23 @@
 package com.riding.todoback.bean;
 
-import com.riding.todoback.entity.CashBoardEntity;
+import com.riding.todoback.bean.Small.ShowPreviewCashBoardDAOBean;
 import com.riding.todoback.model.DTO.RequestPreviewCashBoardAll;
-import com.riding.todoback.repository.CashBoardRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ShowPreviewCashBoardBean {
 
+    ShowPreviewCashBoardDAOBean showPreviewCashBoardDAOBean;
+
     @Autowired
-    CashBoardRepositoryJPA cashBoardRepositoryJPA;
+    public ShowPreviewCashBoardBean(ShowPreviewCashBoardDAOBean showPreviewCashBoardDAOBean) {
+        this.showPreviewCashBoardDAOBean = showPreviewCashBoardDAOBean;
+    }
 
     public List<RequestPreviewCashBoardAll> exec(){
-
-        // cashBoard 데이터를 전부 List 로 저장
-        List<CashBoardEntity> cashBoardEntityList = new ArrayList<>(cashBoardRepositoryJPA.findAll());
-
-        // cashBoard 에 있는 데이터를 옮겨서 저장할 리스트 저장
-        List<RequestPreviewCashBoardAll> requestPreviewCashBoardAllList = new ArrayList<>();
-
-        // 새로운 객체에 데이터를 저장하고 List 에 추가
-        for(CashBoardEntity cashBoardEntity : cashBoardEntityList){
-            RequestPreviewCashBoardAll requestPreviewCashBoardAll = new RequestPreviewCashBoardAll();
-            requestPreviewCashBoardAll.setId(cashBoardEntity.getId());
-            requestPreviewCashBoardAll.setCashData(cashBoardEntity.getCashData());
-            requestPreviewCashBoardAll.setUploadTime(cashBoardEntity.getUploadTime());
-            requestPreviewCashBoardAllList.add(requestPreviewCashBoardAll);
-        }
-
-        // 데이터가 옮겨진 List 를 반환
-        return requestPreviewCashBoardAllList;
+        return showPreviewCashBoardDAOBean.exec();
     }
 }
