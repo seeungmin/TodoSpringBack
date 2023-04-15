4f80d3c61145577e84d468bf669d226cdc6f1233
Lee <lsmin476@naver.com>
2023-04-16 02:27:42 +0900

    |UPDATE| Spring Project board modify refactoring update 0416

--- todoback/src/main/java/com/riding/todoback/bean/ModifyBoardBean.java
+++ todoback/src/main/java/com/riding/todoback/bean/ModifyBoardBean.java
@@ -1,20 +1,37 @@
 package com.riding.todoback.bean;
 
-import com.riding.todoback.bean.Small.ModifyBoardDAOBean;
+import com.riding.todoback.bean.Small.FindByIdDAOBean;
+import com.riding.todoback.bean.Small.ModifyObjectDAOBean;
+import com.riding.todoback.bean.Small.SaveDAOBean;
 import com.riding.todoback.model.DTO.RequestBoardModify;
+import com.riding.todoback.model.entity.BoardEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ModifyBoardBean {
-    ModifyBoardDAOBean modifyBoardDAOBean;
+    FindByIdDAOBean findByIdDAOBean;
+    ModifyObjectDAOBean modifyObjectDAOBean;
+    SaveDAOBean saveDAOBean;
 
     @Autowired
-    public ModifyBoardBean(ModifyBoardDAOBean modifyBoardDAOBean) {
-        this.modifyBoardDAOBean = modifyBoardDAOBean;
+    public ModifyBoardBean(FindByIdDAOBean findByIdDAOBean, ModifyObjectDAOBean modifyObjectDAOBean, SaveDAOBean saveDAOBean) {
+        this.findByIdDAOBean = findByIdDAOBean;
+        this.modifyObjectDAOBean = modifyObjectDAOBean;
+        this.saveDAOBean = saveDAOBean;
     }
 
-    public boolean exec(RequestBoardModify requestBoardModify){
-        return modifyBoardDAOBean.exec(requestBoardModify);
+
+    public Long exec(RequestBoardModify requestBoardModify){
+        // 아이디로 수정할 할 일 찾기
+        BoardEntity boardEntity = findByIdDAOBean.exec(requestBoardModify);
+
+        // 새로 받을 내용 작성
+        BoardEntity modifyBoardEntity = modifyObjectDAOBean.exec(boardEntity, requestBoardModify);
+
+        // 데이터 저장
+        saveDAOBean.exec(modifyBoardEntity);
+
+        return requestBoardModify.getId();
     }
 }
