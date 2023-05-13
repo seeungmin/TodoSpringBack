9fa6ab7b8392095d2f6b8f53be4d92c654f92900
Lee <lsmin476@naver.com>
2023-05-13 17:23:18 +0900

    |ADD| Sprong Project user controller, service add 0513

---
+++ todoback/src/main/java/com/riding/todoback/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.riding.todoback.controller;
+
+import com.riding.todoback.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin("*")
+public class UserController {
+
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
---
+++ todoback/src/main/java/com/riding/todoback/service/UserService.java
@@ -0,0 +1,9 @@
+package com.riding.todoback.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    
+}
