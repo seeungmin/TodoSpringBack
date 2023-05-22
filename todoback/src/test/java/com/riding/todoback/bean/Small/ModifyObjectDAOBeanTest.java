698a298e5b6fb3abc6e4f656dff766d7cd0633b1
Lee <lsmin476@naver.com>
2023-05-23 01:14:35 +0900

    |MAKE| Spring Project Test case modify DAO bean 0523

--- todoback/src/test/java/com/riding/todoback/bean/Small/ModifyObjectDAOBeanTest.java
+++ todoback/src/test/java/com/riding/todoback/bean/Small/ModifyObjectDAOBeanTest.java
@@ -1,6 +1,9 @@
 package com.riding.todoback.bean.Small;
 
+import com.riding.todoback.model.DTO.RequestBoardModify;
 import com.riding.todoback.model.DTO.RequestTodoModify;
+import com.riding.todoback.model.entity.BoardEntity;
+import com.riding.todoback.model.entity.CashBoardEntity;
 import com.riding.todoback.model.entity.TodoEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -40,10 +43,41 @@ class ModifyObjectDAOBeanTest {
 
     @Test
     void modify_board() {
-        
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(1L);
+        boardEntity.setUserId("1");
+        boardEntity.setTitle("테스트");
+        boardEntity.setContent("이 내용은 테스트 입니다.");
+        boardEntity.setModifyTime(LocalDateTime.now());
+        boardEntity.setUploadTime(LocalDateTime.now());
+
+        RequestBoardModify requestBoardModify = new RequestBoardModify();
+        requestBoardModify.setId(1L);
+        requestBoardModify.setTitle("테스트테스트");
+        requestBoardModify.setContent("이 내용은 수정된 내용 입니다.");
+        requestBoardModify.setModifyTime(LocalDateTime.now());
+        modifyObjectDAOBean.exec(boardEntity, requestBoardModify);
+
+        assertThat(boardEntity.getContent()).isEqualTo(requestBoardModify.getContent());
     }
 
     @Test
     void modify_cash_board() {
+        CashBoardEntity cashBoardEntity = new CashBoardEntity();
+        cashBoardEntity.setId(1L);
+        cashBoardEntity.setUserId("1");
+        cashBoardEntity.setTitle("테스트");
+        cashBoardEntity.setCashData("이 내용은 테스트 입니다.");
+        cashBoardEntity.setModifyTime(LocalDateTime.now());
+        cashBoardEntity.setUploadTime(LocalDateTime.now());
+
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setTitle("제목 테스트");
+        boardEntity.setContent("수정 완료한 내용");
+
+        modifyObjectDAOBean.exec(cashBoardEntity, boardEntity);
+
+        assertThat(cashBoardEntity.getCashData()).isEqualTo(boardEntity.getContent());
+        assertThat(cashBoardEntity.getTitle()).isEqualTo(boardEntity.getTitle());
     }
 }
